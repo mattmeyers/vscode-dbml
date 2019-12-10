512c47cffb4436621fdef65a4bb9ac2dc2ee43db
Matt Meyers <mmeyers.07.95@gmail.com>
2019-12-10 01:04:39 -0500

    Split export into class, add default output name

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,18 @@
 import * as vscode from "vscode";
-//@ts-ignore
-import {exporter} from "@dbml/core";
-import * as fs from 'fs';
-import * as path from 'path';
+import Exporter from "./export";
 
-
-enum Dialects {
-  postgresql = "PostgreSQL",
+export enum Dialect {
+  postgres = "PostgreSQL",
   mysql = "MySQL",
-  sqlserver = "SQL Server"
+  mssql = "SQL Server"
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  const exporter = new Exporter();
+
   const exportCmd = vscode.commands.registerCommand(
     "vscode-dbml.export",
-    async () => {
-      if (!vscode.window.activeTextEditor) {
-        return;
-      }
-
-      const infile: string = vscode.window.activeTextEditor.document.fileName;
-
-      const dialect: string | undefined = await vscode.window.showQuickPick(
-        Object.values(Dialects), {placeHolder: "Select an output dialect"}
-      );
-      if (dialect === undefined) {
-        return;
-      }
-
-      let outfile: string | undefined = await vscode.window.showInputBox({prompt: "Enter an output file", value: "out.sql"});
-      if (outfile === undefined) {
-        return;
-      }
-
-      if (!path.isAbsolute(outfile)) {
-        const p = path.dirname(infile);
-        outfile = `${p}/${outfile}`;
-      }
-
-      switch (dialect) {
-        case Dialects.postgresql:
-          const dbml = fs.readFileSync(infile, 'utf-8');
-          const postgres = exporter.export(dbml, 'postgres');
-          fs.writeFileSync(outfile, postgres);
-          break;
-        case Dialects.mysql:
-          vscode.window.showInformationMessage(dialect);
-          break;
-        case Dialects.sqlserver:
-          vscode.window.showInformationMessage(dialect);
-          break;
-      }
-    }
+    () => exporter.export()
   );
 
   context.subscriptions.push(exportCmd);
