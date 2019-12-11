6c753cc11469ab1214e6e4a7b0639f2e37e8a6d9
Matt Meyers <mmeyers.07.95@gmail.com>
2019-12-11 01:11:16 -0500

    Add import command

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import Exporter from "./export";
+import Importer from "./import";
 
 export enum Dialect {
   postgres = "PostgreSQL",
@@ -9,13 +10,12 @@ export enum Dialect {
 
 export function activate(context: vscode.ExtensionContext) {
   const exporter = new Exporter();
+  const importer = new Importer();
 
-  const exportCmd = vscode.commands.registerCommand(
-    "vscode-dbml.export",
-    () => exporter.export()
-  );
+  const exportCmd = vscode.commands.registerCommand("vscode-dbml.export", () => exporter.export());
+  const importCmd = vscode.commands.registerCommand("vscode-dbml.import", () => importer.import());
 
-  context.subscriptions.push(exportCmd);
+  context.subscriptions.push(exportCmd, importCmd);
 }
 
 export function deactivate() {}
